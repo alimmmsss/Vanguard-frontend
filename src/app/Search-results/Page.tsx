0146952219c0b342c3057eeda8aa3f6338befe45
unknown <alim111973@gmail.com>
2025-12-01 00:56:19 +0600

    Added search results page

---
+++ src/app/Search-results/Page.tsx
@@ -0,0 +1,9 @@
+import SearchResults from '../../components/search/SearchResults';
+
+export default function SearchResultsPage() {
+    return (
+        <div className="min-h-screen bg-gray-50">
+            <SearchResults />
+        </div>
+    );
+}
